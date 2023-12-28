5d70df369fb71140ce3a410abc07767e09a35d63
takoyakki0609 <kjy6478@naver.com>
2023-12-28 10:53:21 +0900

    first commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface Porps {
+  text: string;
+}
+
+const Button = ({ text }: Porps) => {
+  return <Buttons>{text}</Buttons>;
+};
+
+export default Button;
+
+const Buttons = styled.button`
+  border: none;
+  background-color: transparent;
+  padding: 0.5rem;
+  cursor: pointer;
+`;
--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,15 +1,13 @@
-import React from "react";
+import Home from 'page/Home';
+import React from 'react';
 // 1. react-router-dom을 사용하기 위해서 아래 API들을 import 합니다.
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-                
-        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
