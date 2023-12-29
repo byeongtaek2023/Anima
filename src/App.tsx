301e1c64a11c99985de2bd88c645f7d5c652e942
takoyakki0609 <kjy6478@naver.com>
2023-12-29 19:07:07 +0900

    crud

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
 import GlobalStyles from 'style/Globalstyles';
 import Router from 'shared/Router';
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient(process.env.REACT_APP_SUPABASE_URL!, process.env.REACT_APP_SUPABASE_ANON_KEY!);
 
 const queryClient = new QueryClient();
 function App() {
